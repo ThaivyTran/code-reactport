856a703e41d7d3e1538d05796556fe254fc26a25
ThaivyTran <Tran.thaivy1997@gmail.com>
2023-02-05 22:46:53 -0500

    edited bio

--- src/routes/About.js
+++ src/routes/About.js
@@ -11,7 +11,7 @@ function About() {
     <p><b>Likes:</b> Tech, Auto, Sneakers, Travel and Noodles </p>
     <p><b>Dislikes:</b> Early mornings, Pumping gas, stepping on Legos </p>
 
-    <p>Hello, World! Welcome to my coding portfolio webpage! I am Thaivy (ta-vy) and here's an introduction. I graduated high school in 2015, after I enlisted into the National Guard Army for 6 years. My MOS was 88M (heavy equipment operator), to keep it short basically I operate heavy up-amored vehicles. In July 2021, I was honorably discharged and now I'm looking at different career paths. Thus, is why I am currently enrolled in uPenn Full Stack boot camp part-time to advance my career.</p>
+    <p>Hello, World! Welcome to my coding react portfolio webpage! My name is Thaivy (ta-vy) and here's an introduction. I graduated high school in 2015, after I enlisted into the National Guard Army for 6 years. My MOS was 88M (heavy equipment operator), to keep it short basically I operate heavy up-amored vehicles. In July 2021, I was honorably discharged and now I'm looking at different career paths. Thus, is why I am currently enrolled in uPenn Full Stack boot camp part-time to advance my career.</p>
     <p>On my days off I enjoy touching grass and exploring new destinations. It could be road trips, flying or by train, as long as we get to stop and explore our surroundings! When I'm not outside, I'm inside watching crime and business documentaries on YouTube or Netflix. Got any recommendations? Let me know!
     </p>
   </section>
