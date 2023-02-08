c0c3547798747e9f47d642574785c5f52a9c53b7
ThaivyTran <Tran.thaivy1997@gmail.com>
2023-02-08 06:07:45 -0500

    Validate email working

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,46 +1,107 @@
 import React from 'react';
 import '../styles/App.css';
+import { useState } from "react";
+import { validateEmail } from "../utils/validateEmail";
 
-const Contact = () => {
+function Contact() {
+	const [ formState, setFormState ] = useState({
+		name: "",
+		email: "",
+		message: "",
+	});
+
+	const [errorMessage, setErrorMessage] = useState("");
+
+	const { name, email, message } = formState;
+
+	function handleChange(e) {
+		if (e.target.name === "email") {
+			const isValid = validateEmail(e.target.value);
+			if (!isValid) {
+				setErrorMessage("Your email is invalid.");
+			} else {
+				if (!e.target.value.length) {
+					setErrorMessage(`${e.target.name} is required.`);
+				} else {
+					setErrorMessage("");
+				}
+			}
+		}
+
+		if (!errorMessage) {
+			setFormState({ ...formState, [e.target.name]: e.target.value });
+		}
+	}
+
+	function handleBlank(e) {
+		if (e.target.name === "Name" || e.target.name === "Message") {
+			if (!e.target.value.length) {
+				setErrorMessage(`${e.target.name} is required.`);
+			} else {
+				setErrorMessage("");
+			}
+		}
+
+		if (!errorMessage) {
+			setFormState({ ...formState, [e.target.name]: e.target.value });
+		}
+	}
+
   const [formStatus, setFormStatus] = React.useState('Send')
   const onSubmit = (e) => {
     e.preventDefault()
-    setFormStatus('Submitting...')
+    setFormStatus('Submitted!')
     const { name, email, message } = e.target.elements
-    let conFom = {
+    let formValue = {
       name: name.value,
       email: email.value,
       message: message.value,
     }
-    console.log(conFom)
+    console.log(formValue)
   }
-  return (
+
+	return (
     <div className="container mt-5">
       <h1 className="mb-3">- Contact -</h1>
       <form onSubmit={onSubmit}>
         <div className="mb-3">
-          <label className="form-label" htmlFor="name">
-            Full Name :
-          </label>
-          <input className="form-control" type="text" id="name" required />
+          <label htmlFor="name">Name :</label>
+          <br></br>
+          <input
+							type="text"
+							defaultValue={name}
+							onBlur={handleBlank}
+							name="Name"
+						/>
+            <br></br>
         </div>
         <div className="mb-3">
-          <label className="form-label" htmlFor="email">
-            Email :
-          </label>
-          <input className="form-control" type="email" id="email" required />
+          <label htmlFor="email">Email :</label>
+          <br></br>
+          <input
+							type="email"
+							defaultValue={email}
+							name="email"
+							onBlur={handleChange}
+						/>
+            <br></br>
         </div>
+        <br></br>
         <div className="mb-3">
-          <label className="form-label" htmlFor="message">
-            Message :
-          </label>
-          <textarea className="form-control" id="message" required />
+          <label htmlFor="message">Message :</label>
+          <br></br>
+          <textarea name="Message"
+							defaultValue={message}
+							onBlur={handleBlank}/>
         </div>
-        <button className="btn btn-primary" type="submit">
-          {formStatus}
-        </button>
-      </form>
-      <br></br>
+        {errorMessage && (
+						<div>
+							<p className="error-text">{errorMessage}</p>
+						</div>
+					)}
+					<button type="submit" class="btn btn-primary"> {formStatus}</button>
+        <br></br>
+        </form>
     </div>
   )
 }
